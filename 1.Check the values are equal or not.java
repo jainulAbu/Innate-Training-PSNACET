55247a5bb41f90737d53e499a12bab4062b1a681
Jainul Abudeen <jainulabudeen60@gmail.com>
2025-07-30 22:47:09 +0530

    Create 1.Check the values are equal or not.java

---
+++ 1.Check the values are equal or not.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner op = new Scanner(System.in);
+        int a = op.nextInt();
+        int b = op.nextInt();
+        if(a==b){
+            System.out.println("EQUAL");
+        }
+        else{
+            System.out.println("NOT EQUAL");
+        }
+        
+    }
+}
