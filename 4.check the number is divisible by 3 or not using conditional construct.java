3817cc587c26f4d57d5292e84d305504e23319d4
Jainul Abudeen <jainulabudeen60@gmail.com>
2025-07-30 22:50:44 +0530

    Create 4.check the number is divisible by 3 or not using conditional construct.java

---
+++ 4.check the number is divisible by 3 or not using conditional construct.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         Scanner op = new Scanner(System.in);
+        int a = op.nextInt();
+        
+        if(a%3==0){
+            System.out.println("The number is divisible by 3");
+        }
+        else{
+            System.out.println("The number is not divisible by 3 and gives a remainder "+a%3);
+        }
+    }
+}
