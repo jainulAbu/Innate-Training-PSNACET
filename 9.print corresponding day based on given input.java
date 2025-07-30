5070f0968dd32e85b5adc5f54e45a31545f2439f
Jainul Abudeen <jainulabudeen60@gmail.com>
2025-07-30 23:06:42 +0530

    Create 9.print corresponding day based on given input.java

---
+++ 9.print corresponding day based on given input.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String day;
+        switch(n){
+            case 0:
+                day = "Sunday";
+                break;
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day = "Saturday";
+                break;
+            default:
+                day ="Invalid";
+                
+        }
+        System.out.println(day);
+    }
+}
